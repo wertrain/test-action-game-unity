11b6f11db0104a0a3b131c544130584f71ee2e90
wertrain <wertrain@gmail.com>
2016-12-07 14:42:21 +0900

    カメラ方向に合わせて移動するように変更

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+        // 自分自身とtargetとの相対距離を求める
+        offset = GetComponent<Transform>().position - target.position;
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        GetComponent<Transform>().position = target.position + offset;
+    }
+}
