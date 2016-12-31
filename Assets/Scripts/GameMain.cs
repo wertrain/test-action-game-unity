9ac4f754c188eea1c29d28a74c7428f31cbb243c
wertrain <wertrain@gmail.com>
2016-12-31 12:09:31 +0900

    ブロックの自動生成を追加

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class GameMain : MonoBehaviour
 {
     private EnemyFactory enemyFactory;
-    private float respawnTime;
+    private float enemyRespawnTime;
     public GameObject enemySource;
 
     private BlockFactory blockFactory;
+    private float blockCreateTime;
     public GameObject blockSource;
+
 
     // Use this for initialization
     void Start ()
     {
         enemyFactory = new EnemyFactory();
         enemyFactory.SetSource(enemySource);
-        respawnTime = Time.time;
+        enemyRespawnTime = Time.time;
 
         blockFactory = new BlockFactory();
         blockFactory.SetSource(blockSource);
@@ -24,10 +26,16 @@ public class GameMain : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-        if (respawnTime + 3.0f < Time.time)
+        if (enemyRespawnTime + 3.0f < Time.time)
         {
             //enemyFactory.Put(new Vector3(Random.value * 8.0f - Random.value * 8.0f, 0, Random.value * 8.0f - Random.value * 8.0f));
-            respawnTime = Time.time;
+            enemyRespawnTime = Time.time;
+        }
+
+        if (blockCreateTime + 3.0f < Time.time)
+        {
+            blockFactory.Put(new Vector3(Random.value * 8.0f - Random.value * 8.0f, 0, Random.value * 8.0f - Random.value * 8.0f));
+            blockCreateTime = Time.time;
         }
     }
 }
